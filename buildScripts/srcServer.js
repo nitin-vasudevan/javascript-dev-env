9190a609d4c9495f4fc11ec0eca352cbc4e36e97
Nitin Vasudevan <nitin.vasudevan@replicon.com>
2018-05-31 16:30:17 +0530

    Added file

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var open = require ('open');
 
-var port = 3000;
+var port = 3502;
 var app = express();
 
 app.get ('/', function(req, res) {
